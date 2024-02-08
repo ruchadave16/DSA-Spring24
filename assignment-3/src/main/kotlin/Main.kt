a9d626f1b38859ced54c35cb8f7d979e8b9d1d61
Rucha Dave <ruchadave16@gmail.com>
2024-02-08 10:39:45 -0500

    Create base files for assignment 3

---
+++ assignment-3/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package org.dsa.assignment3
+
+fun main() {
+    println("Hello World!")
+}
---
+++ assignment-3/src/test/kotlin/MinHeapTest.kt
@@ -0,0 +1,49 @@
+import org.dsa.assignment3.MinHeap
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MinHeapTest {
+
+    @Test
+    fun insert() {
+        val heap = MinHeap<String>()
+        heap.insert(data="test", heapNumber=3.2)
+        heap.insert(data="booh", heapNumber=5.3)
+        heap.insert(data="claw", heapNumber=1.0)
+        heap.insert(data="tester", heapNumber=0.2)
+        heap.insert(data="qq", heapNumber=0.5)
+        heap.insert(data="anotherone", heapNumber=100.0)
+        assertFalse(heap.insert(data="qq", heapNumber=40.0))
+        assertEquals(heap.getMin(), "tester")
+        assertEquals(heap.getMin(), "qq")
+        assertEquals(heap.getMin(), "claw")
+        assertEquals(heap.getMin(), "test")
+        assertEquals(heap.getMin(), "booh")
+        heap.insert(data="reentry", heapNumber=1000.0)
+        assertEquals(heap.getMin(), "anotherone")
+        assertEquals(heap.getMin(), "reentry")
+        assertEquals(heap.getMin(), null)
+    }
+
+    @Test
+    fun adjustPriority() {
+        val heap = MinHeap<String>()
+        heap.insert(data="test", heapNumber=3.2)
+        heap.insert(data="booh", heapNumber=5.3)
+        heap.insert(data="claw", heapNumber=1.0)
+        heap.insert(data="tester", heapNumber=0.2)
+        heap.adjustHeapNumber("booh", 0.0)
+        assertEquals(heap.getMin(), "booh")
+    }
+
+    @Test
+    fun isEmpty() {
+        val heap = MinHeap<String>()
+        assertTrue(heap.isEmpty())
+        heap.insert("test", 30.0)
+        assertFalse(heap.isEmpty())
+        heap.getMin()
+        assertTrue(heap.isEmpty())
+    }
+}
